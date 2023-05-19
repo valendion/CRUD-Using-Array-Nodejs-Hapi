8088a4b54f09bf89682fa1d735d974f3dd24fac0
valendion <valendion874@gmail.com>
2023-05-19 11:42:04 +0800

    collaboration implemented

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,19 @@
-const mapDBToModel = ({ id, title, body, tags, create_at, update_at }) => ({
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  create_at,
+  update_at,
+  username,
+}) => ({
   id,
   title,
   body,
   tags,
   createAt: create_at,
   updateAt: update_at,
+  username,
 });
 
 module.exports = { mapDBToModel };
